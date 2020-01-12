cb918dae8b417e79edcad26950d41f9030011710
pramilagm <pmiris.pramila@gmail.com>
2020-01-11 21:17:26 -0800

    sockets first assignments

---
+++ Socket.io/survey_form/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+app.use(express.static(__dirname + "/static"));
+app.set("view engine", "ejs");
+const server = app.listen(8000);
+const io = require("socket.io")(server);
+var counter = 0;
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+// Listen to connection event from the client side
+io.on("connection", function(socket) {
+  //2
+  var random_number = Math.floor(Math.random() * 1000 + 1);
+  //server listens to "form-posting" event
+  socket.on("form-posting", function(data) {
+    socket.emit("update-message", { response: data }); //3
+
+    socket.emit("random-number", { response: random_number });
+  });
+});
